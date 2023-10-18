c50f7803cc98736fde6aef005a70c0e6527ebb01
IvanShaptalov <qwr111792@gmail.com>
2023-10-17 23:32:06 -0700

    Initial Commit

---
+++ Transfer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transfer App
+//
+//  Created by van on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
